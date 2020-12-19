54fe97095250208b085e670089f6e3d6c6a555f9
Brenda Ferrufino <ferrufinob@gmail.com>
2020-12-19 08:52:32 -0500

    Done.

--- db/migrate/20201219004154_add_artist_reference_to_songs.rb
+++ db/migrate/20201219004154_add_artist_reference_to_songs.rb
@@ -1,6 +1,5 @@
 class AddArtistReferenceToSongs < ActiveRecord::Migration[5.0]
   def change
     add_column :songs, :artist_id, :integer
-    add_foreign_key :songs, :artists
   end
 end
--- db/migrate/20201219004449_add_genre_reference_to_songs.rb
+++ db/migrate/20201219004449_add_genre_reference_to_songs.rb
@@ -1,6 +1,5 @@
 class AddGenreReferenceToSongs < ActiveRecord::Migration[5.0]
   def change
     add_column :songs, :genre_id, :integer
-    add_foreign_key :songs, :genres
   end
 end
